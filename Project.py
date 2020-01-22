2162fd478445a1ddfb282bb1f0f8d2464c111d6f
StepanVerzun <stepan.verzun@mail.ru>
2020-01-22 21:34:58 +0700

    without buttons

--- Project.py
+++ Project.py
@@ -1,4 +1,39 @@
 import pygame
+import os
+import sys
 
 pygame.init()
-pygame.quit()
+screen = pygame.display.set_mode((1920, 1080))
+clock = pygame.time.Clock()
+FPS = 60
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    fon = pygame.transform.scale(load_image('fon.jpg'), (1920, 1080))
+    screen.blit(fon, (0, 0))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+start_screen()
